a3c05733feffd7faea40714ac870086c02753228
foresightyj <imyuanjian@gmail.com>
2016-08-01 17:56:30 +0800

    Added console write hello world

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,9 +1,12 @@
-﻿namespace HelloWorld
+﻿using System;
+
+namespace HelloWorld
 {
     internal class Program
     {
         private static void Main(string[] args)
         {
+            Console.WriteLine("Hello world");
         }
     }
 }
